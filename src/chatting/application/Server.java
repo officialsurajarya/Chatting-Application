3061c8f8afadd57ee42aacb0af67639a1435f67c
Suraj Arya <officialsurajarya@gmail.com>
2025-02-14 16:01:55 +0530

    server connected

--- src/chatting/application/Server.java
+++ src/chatting/application/Server.java
@@ -6,24 +6,27 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.text.*;
+import java.net.*;
+import java.io.*;
 
-public class Server extends JFrame implements ActionListener {
-        
+public class Server implements ActionListener {
+    
     JTextField text;
     JPanel a1;
-    Box vertical = Box.createVerticalBox();
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+    static DataOutputStream dout;
     
     Server() {
-        setLayout(null);
         
-        //Header Panel (Top Bar)
+        f.setLayout(null);
+        
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(7, 94, 84));
         p1.setBounds(0, 0, 450, 70);
         p1.setLayout(null);
-        add(p1);
+        f.add(p1);
         
-        //Back Button (Exit)
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -37,7 +40,6 @@ public class Server extends JFrame implements ActionListener {
             }
         });
         
-        //Profile Image
         ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
         Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
@@ -45,7 +47,6 @@ public class Server extends JFrame implements ActionListener {
         profile.setBounds(40, 10, 50, 50);
         p1.add(profile);
         
-        //Video Call Icon
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
         Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
@@ -53,7 +54,6 @@ public class Server extends JFrame implements ActionListener {
         video.setBounds(300, 20, 30, 30);
         p1.add(video);
         
-        //Phone Call Icon
         ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
         Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
         ImageIcon i12 = new ImageIcon(i11);
@@ -61,84 +61,82 @@ public class Server extends JFrame implements ActionListener {
         phone.setBounds(360, 20, 35, 30);
         p1.add(phone);
         
-        //More Options Icon
         ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
         Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
         ImageIcon i15 = new ImageIcon(i14);
         JLabel morevert = new JLabel(i15);
-        morevert.setBounds(410, 20, 10, 25);
-        p1.add(morevert); 
+        morevert.setBounds(420, 20, 10, 25);
+        p1.add(morevert);
         
-        //Contact Name
         JLabel name = new JLabel("Suraj Arya");
         name.setBounds(110, 15, 100, 18);
         name.setForeground(Color.WHITE);
-        name.setFont(new Font("SAN_SERIF",Font.BOLD, 20));
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
         p1.add(name);
         
-        //Status Label
         JLabel status = new JLabel("Active Now");
-        status.setBounds(110, 40, 100, 18);
+        status.setBounds(110, 35, 100, 18);
         status.setForeground(Color.WHITE);
-        status.setFont(new Font("SAN_SERIF",Font.PLAIN, 15));
+        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
         p1.add(status);
         
-        //Chat Panel
         a1 = new JPanel();
         a1.setBounds(5, 75, 440, 570);
-        add(a1);
+        f.add(a1);
         
-        //Text Field
-        text = new JTextField(20);
+        text = new JTextField();
         text.setBounds(5, 655, 310, 40);
-        text.setFont(new Font ("SAN_SERIF", Font.PLAIN,16));
-        add(text);
+        text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(text);
         
-        //Send Button
         JButton send = new JButton("Send");
         send.setBounds(320, 655, 123, 40);
         send.setBackground(new Color(7, 94, 84));
         send.setForeground(Color.WHITE);
         send.addActionListener(this);
-        send.setFont(new Font ("SAN_SERIF", Font.PLAIN,16));
-        add(send);
+        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(send);
         
-        //Full Frame
-        setSize(450, 700);
-        setLocation(200,50);
-        setUndecorated(true);
-        getContentPane().setBackground(Color.WHITE);
+        f.setSize(450, 700);
+        f.setLocation(200, 50);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
         
-        setVisible(true);
+        f.setVisible(true);
     }
     
-    //Action Perform 
     public void actionPerformed(ActionEvent ae) {
-        String out = text.getText();
-             
-        JPanel p2 = formatLabel(out);
-        
-        a1.setLayout(new BorderLayout());
-        
-        JPanel right = new JPanel (new BorderLayout());
-        right.add(p2, BorderLayout.LINE_END);
-        vertical.add(right);
-        vertical.add(Box.createVerticalStrut(15));
-        
-        a1.add(vertical, BorderLayout.PAGE_START);
-        
-        text.setText("");
-        
-        repaint();
-        invalidate();
-        validate();
+        try {
+            String out = text.getText();
+
+            JPanel p2 = formatLabel(out);
+
+            a1.setLayout(new BorderLayout());
+
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
+
+            a1.add(vertical, BorderLayout.PAGE_START);
+
+            dout.writeUTF(out);
+
+            text.setText("");
+
+            f.repaint();
+            f.invalidate();
+            f.validate();   
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public static JPanel formatLabel(String out) {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         
-        JLabel output = new JLabel("<html><p style='width:150px' >" + out + "</p></html>");
+        JLabel output = new JLabel("<html><p style=\"width: 150px\">" + out + "</p></html>");
         output.setFont(new Font("Tahoma", Font.PLAIN, 16));
         output.setBackground(new Color(37, 211, 102));
         output.setOpaque(true);
@@ -147,7 +145,7 @@ public class Server extends JFrame implements ActionListener {
         panel.add(output);
         
         Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         
         JLabel time = new JLabel();
         time.setText(sdf.format(cal.getTime()));
@@ -159,5 +157,26 @@ public class Server extends JFrame implements ActionListener {
     
     public static void main(String[] args) {
         new Server();
+        
+        try {
+            ServerSocket skt = new ServerSocket(6001);
+            while(true) {
+                Socket s = skt.accept();
+                DataInputStream din = new DataInputStream(s.getInputStream());
+                dout = new DataOutputStream(s.getOutputStream());
+                
+                while(true) {
+                    String msg = din.readUTF();
+                    JPanel panel = formatLabel(msg);
+                    
+                    JPanel left = new JPanel(new BorderLayout());
+                    left.add(panel, BorderLayout.LINE_START);
+                    vertical.add(left);
+                    f.validate();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
